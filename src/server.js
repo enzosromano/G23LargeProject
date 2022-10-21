1913252adadc55b47dddc684366d18f204941925
enzosromano <enzosromano@gmail.com>
2022-10-21 18:48:10 -0400

    test

--- src/server.js
+++ src/server.js
@@ -2,6 +2,4 @@ const app = require("./app");
 const port = 5000;
 const color = require("chalk");
 
-app.listen(port, () =>
-  console.log(`The server is listening on port ${color.green(port)}`)
-);
+app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
