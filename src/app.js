d2879a06fe266c68a3962e789fb0fe6d03ccfcb1
enzosromano <enzosromano@gmail.com>
2022-10-21 20:23:18 -0400

    css fix

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const path = require("path");
 module.exports = app;
 
 app.use(logger("tiny"));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
