268e29ebcfeb0e6d8f1271d63b1ca3095c8f798e
Ethan <bulldawg0065@gmail.com>
2022-10-22 21:49:57 -0400

    Edited index.js to make and demonstrate proof of a connection to the database

--- src/views/index.js
+++ src/views/index.js
@@ -12,16 +12,28 @@ const client = new MongoClient(url);
 async function run() {
     try {
         await client.connect();
-        console.log("Connected correctly to server");
+        console.log("-- CONNECTION ESTABLISHED --\n\n");
+
+        db = client.db('TuneTables')
+
+        var counter1 = await db.collection('counters').findOne({_id : "userID"});
+        var user1 = await db.collection('users').findOne({userID : 1});
+        var song1 = await db.collection('songs').findOne({songID : 1});
+
+        console.log(`first counter: ${counter1.seq}\n`)
+        console.log(`first user:    ${user1.firstName}\n`)
+        console.log(`first song:    ${song1.title}\n`)
+
+        // run the rest of the code
+
+
 
     } catch (err) {
-        console.log(err.stack);
+        console.log(err.stack);// log error
     }
     finally {
-        await client.close();
+        await client.close(); // end connection after process
     }
 }
 
-run().catch(console.dir);
-
-
+run().catch(console.dir); // read error log
