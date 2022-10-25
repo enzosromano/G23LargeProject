0b329feb081648b0aaca09de5002aed8501a5ff2
enzosromano <enzosromano@gmail.com>
2022-10-24 22:01:46 -0400

    moved requests to server.js, refactored variables, added post request shell

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 // app.js
 const express = require("express");
-
 const app = express();
 const logger = require("morgan");
 const path = require("path");
@@ -12,25 +11,3 @@ app.use(logger("tiny"));
 app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 app.use(bodyParser.json());
-
-//START USER VIEWS
-//Initial load on home page, redirects user to index.html
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "index.html"));
-});
-
-//This is for the frontend to use when they make a second page. It will redirect to users.html which you can rename and edit
-app.get("/users", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "users.html"));
-});
-//END USER VIEWS
-
-//START ENDPOINTS
-//TEST ONLY for posting a user. Error output will ALWAYS be empty
-app.post("/users", (req, res) => {
-  const { userId } = req.body;
-  var error = "";
-  var ret = { error: error };
-  res.status(200).json(ret);
-});
-//END ENDPOINTS
--- src/server.js
+++ src/server.js
@@ -1,8 +1,18 @@
-//App Variables
-const app = require("./app");
-const port = 3000;
-const color = require("chalk");
-//
+//const
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const path = require("path");
+
+//exports
+module.exports = app;
+
+//app properties
+app.use(logger("tiny"));
+app.use(express.static(__dirname + "/public"));
+app.use(express.json());
+app.use(bodyParser.json());
 
 //Mongo DB Variables
 const { MongoClient } = require("mongodb");
@@ -42,3 +52,42 @@ async function run() {
 
 run().catch(console.dir); // read error log
 //END TEST
+
+//START USER VIEWS
+//Initial load on home page, redirects user to index.html
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+//This is for the frontend to use when they make a second page. It will redirect to users.html which you can rename and edit
+app.get("/users", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "users.html"));
+});
+//END USER VIEWS
+
+app.post("/users", (req, res) => {
+  const { userID, firstName, lastName, relationships } = req.body;
+
+  const newUser = {
+    UserID: userID,
+    FirstName: firstName,
+    LastName: lastName,
+    Relationships: relationships,
+  };
+
+  var error = "";
+
+  //Error Handling
+  if (!newUser.UserID) {
+    error = "User ID Invalid.";
+    res.status(400).json(error);
+  } else if (!newUser.FirstName || !newUser.LastName) {
+    error = "firstName and lastName are required fields.";
+    res.status(400).json(error);
+  } else if (!newUser.Relationships) {
+    error = "No relationships passed.";
+    res.status(400).json(error);
+  } else {
+    res.status(200).json(newUser);
+  }
+});
