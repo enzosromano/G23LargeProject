f48f487bb9272082bcf00f41201dcb8b085dc200
Ethan <bulldawg0065@gmail.com>
2022-11-10 15:00:19 -0500

    Added a negative outcome test.

--- unit_tests/sum.test.js
+++ unit_tests/sum.test.js
@@ -1,5 +1,11 @@
 const sum = require('./sum');
 
-test('adds 1 + 2 to equal 3', () => {
+
+test('1 + 2 == 3', () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+
+test('1 + 2 != 0', () => {
+  expect(sum(1, 2)).not.toBe(0);
 });
