cf47f73c7ef0bda5e9d0a9e225506e38828fc78a
Ian Frelix <ianfrelix@gmail.com>
2022-11-21 17:50:31 -0500

    created search song api call

--- frontend/src/views/homePage.js
+++ frontend/src/views/homePage.js
@@ -1,10 +1,12 @@
 import React from "react";
+import SearchSongContainer from "../components/HomeComponents/SearchSongContainer";
 
 const HomePage = () => {
 
     return (
         <div>
             <button onClick={() => window.location.href = "/profile"}>Go to your Profile Page!</button>
+            < SearchSongContainer />
         </div>
     )
 }
