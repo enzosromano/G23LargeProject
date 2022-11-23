027eaee8f9c6848c2a402f418680d47ca9baa136
Ian Frelix <ianfrelix@gmail.com>
2022-11-22 19:13:30 -0500

    Login styling up and working

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,24 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+        backgroundImage: {
+            'login_bg': "url('images/login_bg.jpg')",
+            'register_bg': "url('images/register_bg.jpg')",
+        },
+        colors: {
+            brown: {
+                50:  '#DE9E48',
+                100: '#7A431D',
+                200: '#563727',
+                300: '#372C2E',
+                400: '#29191A',
+                500: '#1F1616',
+                600: '#D2735C',
+                700: '#241B19',
+            }   
+        }
+    },
   },
   plugins: [],
 }
