e765854a41e559b6a1bec9e5aa7b02c845269b8d
Ethan <bulldawg0065@gmail.com>
2022-11-23 21:31:36 -0500

    Fixed the test for get_relationships. Updated repop_users to add a friend to user Ewool.

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = async () => {
     return {
         setupFilesAfterEnv: ['<rootDir>/tests/jest.setup.js'],
         verbose: true,
+        "preset": "@shelf/jest-mongodb"
     };
 };
