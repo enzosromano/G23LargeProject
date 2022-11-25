22dcfc113263199fbdfcb277b5b3dae3e766e214
enzosromano <enzosromano@gmail.com>
2022-11-25 10:26:17 -0500

    resolve

--- frontend/src/views/loginPage.js
+++ frontend/src/views/loginPage.js
@@ -5,12 +5,11 @@ import  Login  from "../components/LoginComponents/loginV1";
 const LoginPage = () => {
 
     return (
-        // <div>
-        //     <Login />
-        //     <button onClick={() => window.location.href = "/register"}>Don't have an Account? Register Here!</button>
-        // </div>
         <div>
             <Login />
+            
+            
+            
         </div>
     )
 }
