2bdb83894c07c3683c3ea35980cfff992f3b6fc8
Ian Frelix <ianfrelix@gmail.com>
2022-11-29 18:26:43 -0500

    send email api call

--- frontend/src/components/ProfileComponents/popups/ResetPasswordPopup.jsx
+++ frontend/src/components/ProfileComponents/popups/ResetPasswordPopup.jsx
@@ -4,7 +4,49 @@ import React, { useState } from 'react';
 // not sure if this will be used
 
 function ResetPasswordPopup({active, close}) {
-    
+
+    const app_name = "tunetable23"
+    function buildPath(route) {
+
+        if (process.env.NODE_ENV === "production") { // TECH DEBT PROBLEM
+            return "https://" + app_name + ".herokuapp.com/" + route;
+        }
+        else {
+            return 'http://localhost:5000/' + route;
+        }
+    }
+
+    var email
+    var password
+
+    const [message, setMessage] = useState("");
+
+    const resetSubmit = async event => {
+        event.preventDefault();
+
+        var userID = localStorage.getItem('userID');
+        //First find the friend to delete
+        try {
+            const response = await fetch(buildPath('users/' + userID + '/sendPasswordReset'), { method: 'POST', headers: { 'authorization': 'Bearer ${token}', 'Content-Type': 'application/json' } });
+
+            var res = JSON.parse(await response.text());
+
+            if (!res.success) {
+                setMessage(JSON.stringify(res));
+            }
+            else {
+            
+                setMessage(JSON.stringify(res.message));
+                window.location.reload();
+                
+            }
+        }
+        catch (e) {
+            alert(e.toString());
+            return;
+        }
+
+    }
     
     if (!active) return (null);
     return (
@@ -13,17 +55,15 @@ function ResetPasswordPopup({active, close}) {
                 
                 <div className="mb-4 text-xl">Reset Password</div>
                 {/* <form onSubmit={deleteUserSubmit} className="flex flex-col"> */}
-                <form className="flex flex-col">
-                    <label>Old Password</label>
-                    {/* <input type="password" id="oldPassword" placeholder="Password" ref={(c) => email = c} className="text-gray-700 border-2 relative p-2 mb-4" /> */}
-                    <input type="password" className="text-gray-700 border-2 relative p-2 mb-4" />
-                    <label>New Password</label>
-                    {/* <input type="password" id="newPassword" placeholder="Password" ref={(c) => password = c} className="text-gray-700 border-2 relative p-2 mb-4" /> */}
-                    <input type="password" className="text-gray-700 border-2 relative p-2 mb-4" /> 
+                <form onSubmit={resetSubmit} className="flex flex-col">
+                <label>Email</label>
+                    <input type="text" id="email" placeholder="ianfrelix@yahoo.com" ref={(c) => email = c} className="text-gray-700 border-2 relative p-2 mb-4" />
+                    <label>Password</label>
+                    <input type="password" id="password" placeholder="Password" ref={(c) => password = c} className="text-gray-700 border-2 relative p-2 mb-4" />
                     <div className="flex justify-evenly">
                         <button onClick={close} className="w-1/3 p-1 border border-green-500 bg-green-200 hover:bg-green-400"> cancel </button>
-                        {/* <input type="submit" onClick={deleteUserSubmit} value="yes, reset my password" className="w-1/3 p-1 border border-red-500 bg-red-200 hover:bg-red-600 cursor-pointer"/> */}
-                        <input type="submit" value="reset" className="w-1/3 p-1 border border-red-500 bg-red-200 hover:bg-red-600 cursor-pointer"/>
+                        {/*<input type="submit" onClick={resetSubmit} value="yes, reset my password" className="w-1/3 p-1 border border-red-500 bg-red-200 hover:bg-red-600 cursor-pointer"/>*/}
+                        <input type="submit" onClick={resetSubmit} value="reset" className="w-1/3 p-1 border border-red-500 bg-red-200 hover:bg-red-600 cursor-pointer"/>
                     </div>
                 </form>
                 {/* <span id="resetPasswordResult" className="my-2 text-brown-600">{message}</span> */}
