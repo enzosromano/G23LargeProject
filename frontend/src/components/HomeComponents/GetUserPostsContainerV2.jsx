304643c5e6b8d2d80d9780a42e16d5e76a7c0f22
samuwall <samu.wall02@gmail.com>
2022-11-29 02:40:36 -0500

    attempt 2 at fix home screen

--- frontend/src/components/HomeComponents/GetUserPostsContainerV2.jsx
+++ frontend/src/components/HomeComponents/GetUserPostsContainerV2.jsx
@@ -9,15 +9,15 @@ function GetUserPostsContainerV2() {
 
     const app_name = "tunetable23"
     
-  function buildPath(route) {
+    function buildPath(route) {
 
-      if (process.env.NODE_ENV === "production") { // TECH DEBT PROBLEM
-          return "https://" + app_name + ".herokuapp.com/" + route;
-      }
-      else {
-          return 'http://localhost:5000/' + route;
-       }
-  }
+        if (process.env.NODE_ENV === "production") { // TECH DEBT PROBLEM
+            return "https://" + app_name + ".herokuapp.com/" + route;
+        }
+        else {
+            return 'http://localhost:5000/' + route;
+        }
+    }
     
     const[posts, setPosts] = useState();
     const[message, setMessage] = useState();
@@ -25,68 +25,66 @@ function GetUserPostsContainerV2() {
     useEffect(() => {
         const loadPosts =async () => {
             
-        
-    
-      //Get all user post
-      try {
-          const response = await fetch(buildPath('posts/' + localStorage.getItem('userID')), { method: 'GET', headers: { 'Content-Type': 'application/json' } });
+            //Get all user post
+            try {
+                const response = await fetch(buildPath('posts/' + localStorage.getItem('userID')), { method: 'GET', headers: { 'Content-Type': 'application/json' } });
 
-          var res = JSON.parse(await response.text());
+                var res = JSON.parse(await response.text());
 
-          if (!res.success) {
-              setMessage(JSON.stringify(res));
-          }
-          else {
-                console.log(res.results)
-                
+                if (!res.success) {
+                    setMessage(JSON.stringify(res));
+                }
+                else {
+                    console.log(res.results);
+                        
+                    setMessage(JSON.stringify(res.message));
 
-              setMessage(JSON.stringify(res.message));
+                    setPosts(res.results);
 
-              setPosts(res.results);
-
-              
-                
-          }
-      }
-      catch (e) {
-          alert(e.toString());
-          return;
-      }
+                            
+                }
+            }
+            catch (e) {
+                alert(e.toString());
+                return;
+            }
+            
         };
 
         loadPosts();
     }, []);
     
     
+    if (!posts) return (null);
 
-  return (
-    <div className="Get-User-Post-Container">{
-        !posts ? (<div> No Posts </div>) : (
-            <table className='w-full table-auto'>
-                <thead className='text-white'>
-                    <tr>
-                        <th className='p-3 text-sm font-semibold tracking-wide'>Song</th>
-                        <th className='p-3 text-sm font-semibold tracking-wide'>Artist</th>
-                        <th className='p-3 text-sm font-semibold tracking-wide'>Message</th>
-                        <th className='p-3 text-sm font-semibold tracking-wide'>Likes</th>
-                        <th className='p-3 text-sm font-semibold tracking-wide'>Delete Post</th>
-                    </tr>
-                </thead>
-                <tbody className='text-white'>
-                    {
-                        posts.map((post, index) => (
-                            <tr key={index}>
-                                <td className='p-3 text-sm font-semibold tracking-wide'>{post.song.title}</td>
-                                <td className='p-3 text-sm font-semibold tracking-wide'>{post.song.artist}</td>
-                                <td className='p-3 text-sm font-semibold tracking-wide'>{post.message}</td>
-                                <td className='p-3 text-sm font-semibold tracking-wide'>{post.likes}</td>
-                                <td className='p-3 text-sm font-semibold tracking-wide'><DeletePostContainer postID={post._id}/></td>
-                            </tr>
-                        ))
-                    }
-                </tbody>
-            </table>
-        )}
+    return (
+        <div className="Get-User-Post-Container">{
+            !posts ? (<div> No Posts </div>) : (
+                <table className='w-full table-auto'>
+                    <thead className='text-white'>
+                        <tr>
+                            <th className='p-3 text-sm font-semibold tracking-wide'>Song</th>
+                            <th className='p-3 text-sm font-semibold tracking-wide'>Artist</th>
+                            <th className='p-3 text-sm font-semibold tracking-wide'>Message</th>
+                            <th className='p-3 text-sm font-semibold tracking-wide'>Likes</th>
+                            <th className='p-3 text-sm font-semibold tracking-wide'>Delete Post</th>
+                        </tr>
+                    </thead>
+                    <tbody className='text-white'>
+                        {
+                            posts.map((post, index) => (
+                                <tr key={index}>
+                                    <td className='p-3 text-sm font-semibold tracking-wide'>{post.song.title}</td>
+                                    <td className='p-3 text-sm font-semibold tracking-wide'>{post.song.artist}</td>
+                                    <td className='p-3 text-sm font-semibold tracking-wide'>{post.message}</td>
+                                    <td className='p-3 text-sm font-semibold tracking-wide'>{post.likes}</td>
+                                    <td className='p-3 text-sm font-semibold tracking-wide'><DeletePostContainer postID={post._id}/></td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            )}
         <nav className="d-flex align-content: center">
             <ul className="pagination">
                 {
@@ -97,8 +95,8 @@ function GetUserPostsContainerV2() {
                 
             </ul>
         </nav>
-        </div>
-  )
+    </div>
+    )
 }
 
 export default GetUserPostsContainerV2
